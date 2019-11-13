5bc7c8e9d5802fd77948034fb75852f5fb6c2550
Minh Mai <minhmanmai90@gmail.com>
2019-11-13 20:52:33 +1100

    update game logic

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ GAME RULES:
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The player can choose to 'Keep score', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var score0, score1, current0, current1, roundScore,
-    activePlayer, dice, 
-    btnStart, btnNew, btnRoll, btnHold;
+var score0, score1, current0, current1, currentScore,
+    activePlayer, dice,
+    btnStart, btnNew, btnRoll, btnKeep,
+    winnerMsg;
 const END_SCORE = 100;
 
 score0 = document.querySelector('#score-0');
@@ -21,21 +22,51 @@ dice = document.querySelector('.dice');
 btnStart = document.querySelector('.btn-start');
 btnNew = document.querySelector('.btn-new');
 btnRoll = document.querySelector('.btn-roll');
-btnHold = document.querySelector('.btn-hold');
-roundScore = 0;
+btnKeep = document.querySelector('.btn-keep');
+currentScore = 0;
 activePlayer = 0;
+winnerMsg = document.querySelector('.winner-message');
 
 initGame();
 
-btnStart.addEventListener('click', function(){
+btnStart.addEventListener('click', function () {
     hide(btnStart);
-    show(btnNew, btnRoll, btnHold, dice);
+    show(btnNew, btnRoll, btnKeep, dice);
+})
+
+btnNew.addEventListener('click', function () {
+    initGame();
 })
 
 btnRoll.addEventListener('click', function () {
     diceValue = Math.floor(Math.random() * 6 + 1);
     dice.src = 'dice-' + diceValue + '.png';
-    
+    if (diceValue != 1) {
+        currentScore += diceValue;
+        document.querySelector('#current-' + activePlayer).textContent = currentScore;
+    } else {
+        currentScore = 0;
+        document.querySelector('#current-' + activePlayer).textContent = 0;
+        activePlayer == 0 ? activePlayer = 1 : activePlayer = 0;
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+    }
+
+})
+
+btnKeep.addEventListener('click', function () {
+    totalScore = parseInt(document.querySelector('#score-' + activePlayer).textContent) + currentScore;
+    document.querySelector('#score-' + activePlayer).textContent = parseInt(totalScore);
+    document.querySelector('#current-' + activePlayer).textContent = 0;
+    if (document.querySelector('#score-' + activePlayer).textContent >= 100) {
+        hide(btnRoll, btnKeep, dice);
+        winnerMsg.textContent = "Player " + (activePlayer + 1) + " is the winner!"
+        show(winnerMsg);
+    }else {
+        activePlayer == 0 ? activePlayer = 1 : activePlayer = 0;
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+    }
 })
 
 function resetScore(...args) {
@@ -58,6 +89,6 @@ function hide(...args) {
 
 function initGame() {
     resetScore(score0, score1, current0, current1);
-    hide(btnNew, btnRoll, btnHold, dice);
+    hide(btnNew, btnRoll, btnKeep, dice, winnerMsg);
     show(btnStart);
 }
